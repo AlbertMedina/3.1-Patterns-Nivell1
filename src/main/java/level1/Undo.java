e4e8578241a2b3c0bd3f53c2bc9ebb8efbd651c7
Albert Medina <albertmecu@gmail.com>
2025-11-03 10:50:45 +0100

    feat: implemented Undo class which uses Singleton pattern

---
+++ src/main/java/level1/Undo.java
@@ -0,0 +1,43 @@
+package level1;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Undo {
+
+    private static Undo instance;
+
+    private final List<String> commands;
+
+    public Undo() {
+        commands = new ArrayList<>();
+    }
+
+    public static Undo getInstance() {
+        if (instance == null) {
+            instance = new Undo();
+        }
+        return instance;
+    }
+
+    public void addCommand(String command) {
+        if (command != null && !command.trim().isEmpty()) {
+            commands.add(command);
+            System.out.println("'" + command + "' added");
+        }
+    }
+
+    public void removeCommand() {
+        if (!commands.isEmpty()) {
+            String command = commands.removeLast();
+            System.out.println("'" + command + "' removed");
+        }
+    }
+
+    public String getCommandsList() {
+        StringBuilder sb = new StringBuilder("Commands history:");
+        commands.forEach(c -> sb.append("\n").append("- ").append(c));
+        return sb.toString();
+    }
+}
+
